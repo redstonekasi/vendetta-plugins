4af9d7f91e4c78b4e834d67909b3f3a512faeb4d
redstonekasi <kasi@hanneskann.de>
2023-01-09 04:02:38 +0100

    [dk+] that broke something

--- plugins/devkitplus/src/socket.js
+++ plugins/devkitplus/src/socket.js
@@ -1,18 +1,10 @@
 // gonna make this configurable later
 
-// react-devtools-core auto reconnects every 2 seconds by default
 if (window.__vendetta_rdc) {
-  const { showToast } = vendetta.ui.toasts;
-  const { getAssetIDByName } = vendetta.ui.assets;
   const { StyleSheet } = vendetta.metro.common.ReactNative;
-
-  const ws = new WebSocket("ws://10.0.0.12:8097");
-
-  ws.addEventListener("open", () => void showToast("Connected to React DevTools", getAssetIDByName("Check")));
-  ws.addEventListener("close", () => void showToast("Disconnect from React DevTools", getAssetIDByName("Small")));
-
+  
   window.__vendetta_rdc.connectToDevTools({
-    websocket: ws,
+    host: "10.0.0.12",
     resolveRNStyle: StyleSheet.flatten,
   });
 }

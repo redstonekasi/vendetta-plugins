2aef9a90a73a9bdd8825a5b735125cef32b6f4a5
redstonekasi <kasi@hanneskann.de>
2023-01-12 10:47:11 +0100

    [no-reply-mentions] remove

--- plugins/no-reply-mention/src/index.js
+++
@@ -1,9 +0,0 @@
-const { before } = vendetta.patcher;
-const Reply = vendetta.metro.findByProps("startTyping");
-
-const unpatch = before("createPendingReply", Reply, (args) => {
-  args[0].shouldMention = false;
-  return args;
-});
-
-export const onUnload = () => unpatch();

2e43ebda91fe199e3c4bc91ad17459e5e647e4b1
redstonekasi <redstonekasi@users.noreply.github.com>
2023-01-12 09:47:35 +0000

    deploy: e0486984095a071c9270bde9bff3a68e2b02f3c1

--- no-reply-mention/index.js
+++
@@ -1,1 +0,0 @@
-(function(n){"use strict";const{before:e}=vendetta.patcher,o=vendetta.metro.findByProps("startTyping"),r=e("createPendingReply",o,t=>(t[0].shouldMention=!1,t)),a=()=>r();return n.onUnload=a,n})({});

a6a785b6ad5fea8604e42878848f54df1a9289b3
redstonekasi <kasi@hanneskann.de>
2023-04-05 20:41:46 +0200

    [url-import] vendata plugin discord crash issue

--- plugins/url-import/src/index.js
+++ plugins/url-import/src/index.js
@@ -3,7 +3,9 @@ import { removePlugin } from "@vendetta/plugins";
 import { getAssetIDByName } from "@vendetta/ui/assets";
 import { showToast } from "@vendetta/ui/toasts";
 
-removePlugin(id);
-showToast("URL import has been integrated into Vendetta and thus removed.", getAssetIDByName("ic_information_filled_24px"));
+setTimeout(() => {
+  removePlugin(id);
+  showToast("URL import has been integrated into Vendetta and thus removed.", getAssetIDByName("ic_information_filled_24px"));
+});
 
 export const onUnload = () => {};

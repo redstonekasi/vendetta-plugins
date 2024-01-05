7e22325ce0be459f8f4ca92e951d1285523d198f
Jānis <97699850+janisslsm@users.noreply.github.com>
2024-01-05 16:09:28 +0200

    [audiofix] fix for newer versions of discord

--- plugins/audiofix/src/index.ts
+++ plugins/audiofix/src/index.ts
@@ -1,4 +1,4 @@
 import { ReactNative as RN } from "@vendetta/metro/common";
 import { instead } from "@vendetta/patcher";
 
-export const onUnload = instead("setCommunicationModeOn", RN.NativeModules.AudioManager, () => {});
+export const onUnload = instead("setCommunicationModeOn", RN.NativeModules.AudioManager === null ? RN.NativeModules.RTNAudioManager : RN.NativeModules.AudioManager, () => {});
